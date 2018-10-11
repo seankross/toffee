833b0373891ebcc7ed67ba0fc69ed2ed0461af3e
seankross <smk240@gmail.com>
2018-10-10 20:12:29 -0700

    added gloabl and added lots of docs

---
+++ R/global.R
@@ -0,0 +1,4 @@
+utils::globalVariables(c("family", "make.link", "symnum",
+                         "p.value", "conf.low", "conf.high",
+                         "term", "estimate", "CI", "Significant",
+                         "Estimate", "p_value"))
--- R/toffee_signif.R
+++ R/toffee_signif.R
@@ -1,16 +1,25 @@
-
+#' Specify symbols to represent confidence levels.
+#'
 #' @param p_values A numeric vector of p-values.
-#' @param thesholds Descending p-value thresholds. Values less than or equal to
+#' @param thresholds Descending p-value thresholds. Values less than or equal to
 #'  a threshold will produce the corresponding character.
 #' @param chars A vector of strings that symbolize a p-value less than the
 #' threshold.
+#' @export
+#' @examples
+#'
+#' toffee_signif(c(1, .5, .049, .01, .0012, 0))
 toffee_signif <- function(p_values,
                          thresholds = c(0.05, 0.01, 0.001),
                          chars = c("*", "**", "***")) {
+
+  if(length(thresholds) != length(chars)) {
+    stop("[toffee_signif] Please ensure that there are the same number of thresholds and character symbols.",
+         call. = FALSE)
+  }
+
   symnum(p_values, cutpoints = c(0, rev(thresholds), 1),
          symbols = c(rev(chars), ""),
          corr = FALSE, na = FALSE) %>%
     as.character()
 }
-
-toffee_stars(c(1, .5, .049, .01, .0012, 0))
